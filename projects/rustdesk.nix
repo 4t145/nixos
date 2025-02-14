73d5041c3c953565707c278041ccd4fdd6878eba
4t145 <u4t145@163.com>
2025-02-15 02:47:59 +0800

    fix rustdesk dev env

--- projects/rustdesk.nix
+++ projects/rustdesk.nix
@@ -2,71 +2,67 @@
   description = "rustdesk devShell";
 
   inputs = {
-    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
-    flake-utils.url  = "github:numtide/flake-utils";
+    flake-utils.url = "github:numtide/flake-utils";
+    vcpkg = {
+      url = "github:microsoft/vcpkg";
+      flake = false;
+    };
   };
 
-  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      rust-overlay,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         overlays = [ (import rust-overlay) ];
-        pkgs = import nixpkgs {
-          inherit system overlays;
-        };
+        pkgs = import nixpkgs { inherit system overlays; };
 
         # flutter
-        flutter = pkgs.flutter.override {
-          version = "3.24.5";
-        };
-
-        # build vcpkg
-        vcpkgCommit = "6f29f12e82a8293156836ad81cc9bf5af41fe836";
-        vcpkgDir = "./.vcpkg-${vcpkgCommit}";
-        vcpkg = pkgs.runCommand "vcpkg" {
-          nativeBuildInputs = [ pkgs.git pkgs.cmake pkgs.python3 ];
-        } ''
-          git clone https://github.com/microsoft/vcpkg $out
-          cd $out
-          git checkout ${vcpkgCommit}
-          ./bootstrap-vcpkg.sh -disableMetrics
-        '';
-
+        # flutter = pkgs.flutter.override { version = "3.24.5"; };
 
       in
       {
-        devShells.default = with pkgs; mkShell {
-          buildInputs = [
-            openssl
-            pkg-config
-            eza
-            fd
-            rust-bin.stable."1.75".default
-            flutter
-            python3
-          ];
+        devShells.default =
+          with pkgs;
+          mkShell {
+            buildInputs = [
+              openssl
+              pkg-config
+              eza
+              fd
+              rust-bin.stable."1.75.0".default
+              flutter
+              python3
+              cmake
+              vcpkg
+            ];
 
-          shellHook = ''
-            # cmake
-            echo "CMake toolchain: $CMAKE_TOOLCHAIN_FILE"
+            shellHook = ''
 
-            # vcpkg initialize
-            export VCPKG_ROOT="${vcpkg}"
-            export VCPKG_INSTALLED_ROOT="${vcpkg}/installed"
-            export PATH="$VCPKG_ROOT:$PATH"
-            echo "current vcpkg version: $(git -C $VCPKG_ROOT rev-parse HEAD)"
+              # vcpkg initialize
+              export VCPKG_ROOT="${vcpkg}"
+              export VCPKG_INSTALLED_ROOT="${vcpkg}/installed"
 
-            # flutter
-            echo "Flutter version: $(flutter --version)"
 
-            # rust
-            echo "Rust version: $(rustc --version)"
+              # flutter
+              echo "Flutter version: $(flutter --version)"
 
-            # vcpkg install
-            echo "Installing vcpkg libraries..."
-            vcpkg install --x-install-root="$VCPKG_INSTALLED_ROOT"
-          '';
-        };
+              # rust
+              echo "Rust version: $(rustc --version)"
+
+              # vcpkg install
+              echo "Installing vcpkg libraries..."
+              vcpkg install --x-install-root="$VCPKG_INSTALLED_ROOT"
+            '';
+          };
       }
     );
 }
