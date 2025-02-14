6960243e881349ec680ff1b8cee6130b03e1a945
4t145 <u4t145@163.com>
2025-02-14 21:54:56 +0800

    add plasma 6

---
+++ modules/plasma6.nix
@@ -0,0 +1,30 @@
+{
+  config, 
+  pkgs,
+  username,
+  ...
+} : {
+
+  services.xserver = {
+    enable = true;
+    layout = "us";
+    desktopManager.plasma6.enable = true;
+    displayManager.sddm.enable = true;
+    displayManager = {
+      defaultSession = "plasma";
+      # 自动登录设置
+      lightdm.enable = true;
+      autoLogin = {
+        enable = true;
+        user = username;  # 替换为你的用户名
+      };
+    };
+  
+    # 安装必要的包
+    environment.plasma6.excludePackages = with pkgs.kdePackages; [
+      plasma-browser-integration
+      konsole
+      oxygen
+    ];
+  };
+}
