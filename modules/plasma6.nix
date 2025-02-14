ce388176f40b949c3ceb7cd73b78fb095756d617
4t145 <u4t145@163.com>
2025-02-14 22:13:07 +0800

    fix plasma6 environment

--- modules/plasma6.nix
+++ modules/plasma6.nix
@@ -5,22 +5,18 @@
   environment,
   ...
 } : {
-
+  service.desktopManager = {
+    plasma6.enable = true;
+    defaultSession = "plasma";
+    sddm.enable = true;
+    autoLogin = {
+      enable = true;
+      user = username; 
+    };
+  };
   services.xserver = {
     enable = true;
-    layout = "us";
-    desktopManager.plasma6.enable = true;
-    displayManager.sddm.enable = true;
-    displayManager = {
-      defaultSession = "plasma";
-      # 自动登录设置
-      lightdm.enable = true;
-      autoLogin = {
-        enable = true;
-        user = username;  # 替换为你的用户名
-      };
-    };
-  
+    xkb.layout = "us";
   };
 
   # 安装必要的包
